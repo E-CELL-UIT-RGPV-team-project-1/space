b66236567ae695e6ce3a4b59452746929e55ffd0
sumitchoudharySEM <sumitsem2004@gmail.com>
2022-03-16 16:08:02 +0530

    first commit

---
+++ src/navbar.js
@@ -0,0 +1,15 @@
+import './navbar.css';
+const Navbar = () => {
+    return ( 
+        <div className="Navbar">
+            this is the nav bar
+        </div>
+
+
+     );
+}
+ 
+export default Navbar;
+
+
+
---
+++ src/partners.js
@@ -0,0 +1,57 @@
+import './partners.css';
+
+const Parteners = () => {
+    return (
+        <div classNameName="parteners-box">
+
+            <section className="teams" id="teams">
+                <h2 className="title">sponsers/parteners</h2>
+                <div className="max-width">
+
+                    <div className="row1">
+                        <div className="card">
+                            <div className="box">
+                                <img src={require('./img/moon.png')} alt="" />
+                                <div className="text">Someone name</div>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="box">
+                                <img src={require('./img/moon.png')} alt="" />
+                                <div className="text">Someone name</div>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="box">
+                                <img src={require('./img/moon.png')} alt="" />
+                                <div className="text">Someone name</div>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row2">
+                        <div className="card">
+                            <div className="box">
+                                <img src={require('./img/moon.png')} alt="" />
+                                <div className="text">Someone name</div>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="box">
+                                <img src={require('./img/moon.png')} alt="" />
+                                <div className="text">Someone name</div>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default Parteners;
