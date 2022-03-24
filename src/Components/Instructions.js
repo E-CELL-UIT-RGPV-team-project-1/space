af0474788a7a2762a42f2c4cf8af9f5ec9bd25b0
sumitchoudharySEM <sumitsem2004@gmail.com>
2022-03-24 22:14:27 +0530

    instruction section chainged

--- src/Components/Instructions.js
+++ src/Components/Instructions.js
@@ -3,6 +3,7 @@ import Registration from './Registration';
 import Faq from './Faq';
 import PastEvents from './PastEvents';
 import AnimatedPage from "./AnimatedPage";
+import "./instructions.css";
 
 
 
@@ -10,7 +11,69 @@ const Instructions = () => {
   return (
     <AnimatedPage>
       <>
-    <div style={{ backgroundColor: '#0c081c' }}>
+
+        <div className="inst_container" style={{ color: 'white', backgroundColor: '#0c081c' }}>
+          <h1 className="Heading">instructions</h1>
+          <div className="row_inst1">
+            <div className="inst">
+              <div style={{ marginBottom: 15 }} className="pngtag">
+                <i style={{ height: 20, width: 20, fontSize: 35, }} class="fa-solid fa-user"></i>
+              </div>
+              <div className="inst-detail">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas provident repudiandae aperiam id sunt hic excepturi eligendi quas omnis aut.
+              </div>
+            </div>
+            <div className="inst">
+              <div style={{ marginBottom: 15 }} className="pngtag">
+                <i style={{ height: 20, width: 20, fontSize: 35, }} class="fa-solid fa-code"></i>
+              </div>
+              <div className="inst-detail">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas provident repudiandae aperiam id sunt hic excepturi eligendi quas omnis aut.
+              </div>
+            </div>
+            <div className="inst">
+              <div style={{ marginBottom: 15 }} className="pngtag">
+                <i style={{ height: 20, width: 20, fontSize: 35, }}  class="fa-solid fa-code-merge"></i>
+              </div>
+              <div className="inst-detail">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas provident repudiandae aperiam id sunt hic excepturi eligendi quas omnis aut.
+              </div>
+            </div>
+          </div>
+         
+           <div className="row_inst1">
+            <div className="inst">
+              <div style={{ marginBottom: 15 }} className="pngtag">
+                <i style={{ height: 20, width: 20, fontSize: 35, }} class="fa-solid fa-cubes"></i>
+              </div>
+              <div className="inst-detail">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas provident repudiandae aperiam id sunt hic excepturi eligendi quas omnis aut.
+              </div>
+            </div>
+            <div className="inst">
+              <div style={{ marginBottom: 15 }} className="pngtag">
+                <i style={{ height: 20, width: 20, fontSize: 35, }}class="fa-solid fa-laptop-code"></i>
+              </div>
+              <div className="inst-detail">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas provident repudiandae aperiam id sunt hic excepturi eligendi quas omnis aut.
+              </div>
+            </div>
+            <div className="inst">
+              <div style={{ marginBottom: 15 }} className="pngtag">
+                <i style={{ height: 20, width: 20, fontSize: 35, }} class="fa-solid fa-user-secret"></i>
+              </div>
+              <div className="inst-detail">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas provident repudiandae aperiam id sunt hic excepturi eligendi quas omnis aut.
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+
+
+        {/* <div style={{ backgroundColor: '#0c081c' }}>
       <br />
       <p className='hi text-info mt-2 mb-4 Heading2' style={{ Color: '#eee', fontSize: '3rem', textAlign: 'center', paddingtop: '4rem' }}>
         INSTRUCTIONS
@@ -40,14 +103,14 @@ const Instructions = () => {
           </div>
         </div></div>
 
-    </div>
+    </div> */}
 
-    <Registration />
-    <Faq />
-    <PastEvents />
+        <Registration />
+        <Faq />
+        <PastEvents />
 
-  </>
-  </AnimatedPage>
+      </>
+    </AnimatedPage>
 
   )
 }
